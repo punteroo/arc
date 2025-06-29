2ba3b1560c7c4c645d1c08df302ee3c7d269858e
Lucas <mazalucas@hotmail.com>
2025-06-28 23:22:18 -0300

    feat: axios, session management, test login page

--- auth.ts
+++ auth.ts
@@ -1,6 +1,15 @@
 import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
 
+// Modified Discord provider to add more scopes.
+const ArcDiscordProvider = Discord({
+  authorization: {
+    params: {
+      scope: "identify email guilds",
+    },
+  },
+});
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Discord],
+  providers: [ArcDiscordProvider],
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
 export default function Home() {
-  return null;
+  // Test discord sign-in
+  return (
+    <main
+      className="flex min-h-screen flex-col items-center justify-between p-24
+"
+    >
+      <h1 className="text-4xl font-bold">Welcome to Arc!</h1>
+      <p className="mt-4 text-lg">This is a custom hunger-games client.</p>
+      <a
+        className="mt-6 inline-block rounded bg-blue-600 px-6 py-3 text-white hover:bg-blue-700"
+        href="#"
+        onClick={() => signIn("discord")}
+      >
+        Sign in with Discord
+      </a>
+      <p className="mt-4 text-sm text-gray-500">
+        This is a test page for the Arc client. It is not functional yet.
+      </p>
+    </main>
+  );
 }
