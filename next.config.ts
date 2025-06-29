67709ea0f40129830a435ade92bd63609e129737
Lucas <mazalucas@hotmail.com>
2025-06-29 19:32:43 -0300

    feat: heroui component library, discord bot integration envs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
+import webpack from "webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({ resourceRegExp: /^zlib-sync$/ }),
+      new webpack.IgnorePlugin({ resourceRegExp: /^bufferutil$/ })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
